481b85edf82b6e33fa7d7a4802c85cdd44374e70
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-25 20:26:08 +0530

    dt

--- Aug25Exercises/Program.cs
+++ Aug25Exercises/Program.cs
@@ -5,84 +5,28 @@ namespace Aug25Exercises
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            int flag = 0;
-            do
+        
+        
+            static void Main(string[] args)
             {
-                Console.Write("1.ADD\n2.APPEND\n3.TRUNCATE\n4.READ\n");
-                string input = Console.ReadLine();
-                Console.WriteLine("EnterFilename");
-                string p = Console.ReadLine();
-                switch (input)
+                Console.WriteLine("C drive info");
+                DriveInfo info = new DriveInfo("C");
+                Console.WriteLine(info.Name);
+                Console.WriteLine(info.TotalSize);
+                Console.WriteLine(info.DriveType);
+                Console.WriteLine(info.DriveFormat);
+                Console.WriteLine("----------------------------");
+                DriveInfo[] dinfo = DriveInfo.GetDrives();
+                foreach (var item in dinfo)
                 {
-                    case "ADD":
-                        if (File.Exists(p))
-                        {
-                            Console.WriteLine("File already exists");
-                        }
-                        else
-                        {
-
-                            FileStream fs = new FileStream(p, FileMode.CreateNew, FileAccess.Write);
-                            StreamWriter sw = new StreamWriter(fs);
-                            sw.WriteLine("I am sai vignesh");
-                            fs.Flush();
-                            sw.Close();
-                            fs.Close();
-                            sw.Dispose();
-                            fs.Dispose();
-                        }
-                        break;
-                    case "APPEND":
-                        FileStream gs = new FileStream(p, FileMode.Append, FileAccess.Write);
-                        StreamWriter gw = new StreamWriter(gs);
-                        gw.WriteLine("I am saivignesh");
-                        gs.Flush();
-                        gw.Close();
-                        gs.Close();
-                        gw.Dispose();
-                        gs.Dispose();
-
-                        break;
-                    case "TRUNCATE":
-                        FileStream ps = new FileStream(p, FileMode.Truncate, FileAccess.Write);
-                        StreamWriter pw = new StreamWriter(ps);
-                        pw.WriteLine(" ");
-                        ps.Flush();
-                        pw.Close();
-                        ps.Close();
-                        pw.Dispose();
-                        ps.Dispose();
-
-                        break;
-                    case "READ":
-                        if (!File.Exists(p))
-                        {
-                            Console.Write("File does not exist");
-                        }
-                        else
-                        {
-                            FileStream ms = new FileStream(p, FileMode.Open, FileAccess.Read);
-                            StreamReader mw = new StreamReader(ms);
-                            string h = mw.ReadToEnd();
-                            Console.WriteLine(h);
-
-                            ms.Flush();
-                            mw.Close();
-                            ms.Close();
-                            mw.Dispose();
-                            ms.Dispose();
-                        }
-                        break;
-                    default:
-                        break;
+                    Console.WriteLine(item.Name);
                 }
-                Console.WriteLine("Enter 1 to Continue");
-                int k = Convert.ToInt32(Console.ReadLine());
-                flag = k;
-            } while (flag != 0);
-            Console.ReadLine();
-        }
+
+                Console.ReadLine();
+            
+        
+    
+
+}
     }
 }
