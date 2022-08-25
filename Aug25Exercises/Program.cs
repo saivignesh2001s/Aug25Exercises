0ab19ef3804ae66ef854c3559c25b1c375428a64
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-25 16:01:34 +0530

    cd

--- Aug25Exercises/Program.cs
+++ Aug25Exercises/Program.cs
@@ -7,82 +7,62 @@ namespace Aug25Exercises
     {
         static void Main(string[] args)
         {
-            int flag = 0;
-            do
+           
+           
+            Console.Write("1.Copy in same folder\n2.Copy in diff folder\n3.Delete\n4.Move\n");
+            int input=Convert.ToInt32(Console.ReadLine());
+            switch (input)
             {
-                Console.Write("1.ADD\n2.APPEND\n3.TRUNCATE\n4.READ\n");
-                string input = Console.ReadLine();
-                Console.WriteLine("EnterFilename");
-                string p = Console.ReadLine();
-                switch (input)
-                {
-                    case "ADD":
-                        if (File.Exists(p))
-                        {
-                            Console.WriteLine("File already exists");
-                        }
-                        else
-                        {
+                case 1:
+                    string s =@"C:\Users\saivignesh\";
+                    Console.WriteLine("Enter File Names");
+                    string m=Console.ReadLine();
+                    string t=string.Concat(s,m);
+                    string p=Console.ReadLine();
+                    string h=string.Concat(s,p);
+                    File.Copy(t,h);
+                    FileInfo g = new FileInfo(h);
+                    Console.WriteLine(g.Extension);
+                    Console.WriteLine(g.CreationTime);
+                    Console.WriteLine(g.DirectoryName);
 
-                            FileStream fs = new FileStream(p, FileMode.CreateNew, FileAccess.Write);
-                            StreamWriter sw = new StreamWriter(fs);
-                            sw.WriteLine("I am sai vignesh");
-                            fs.Flush();
-                            sw.Close();
-                            fs.Close();
-                            sw.Dispose();
-                            fs.Dispose();
-                        }
-                        break;
-                    case "APPEND":
-                        FileStream gs = new FileStream(p, FileMode.Append, FileAccess.Write);
-                        StreamWriter gw = new StreamWriter(gs);
-                        gw.WriteLine("I am saivignesh");
-                        gs.Flush();
-                        gw.Close();
-                        gs.Close();
-                        gw.Dispose();
-                        gs.Dispose();
-
-                        break;
-                    case "TRUNCATE":
-                        FileStream ps = new FileStream(p, FileMode.Truncate, FileAccess.Write);
-                        StreamWriter pw = new StreamWriter(ps);
-                        pw.WriteLine(" ");
-                        ps.Flush();
-                        pw.Close();
-                        ps.Close();
-                        pw.Dispose();
-                        ps.Dispose();
-
-                        break;
-                    case "READ":
-                        if (!File.Exists(p))
-                        {
-                            Console.Write("File does not exist");
-                        }
-                        else
-                        {
-                            FileStream ms = new FileStream(p, FileMode.Open, FileAccess.Read);
-                            StreamReader mw = new StreamReader(ms);
-                            string h = mw.ReadToEnd();
-                            Console.WriteLine(h);
-
-                            ms.Flush();
-                            mw.Close();
-                            ms.Close();
-                            mw.Dispose();
-                            ms.Dispose();
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                Console.WriteLine("Enter 1 to Continue");
-                int k = Convert.ToInt32(Console.ReadLine());
-                flag = k;
-            } while (flag != 0);
-            Console.ReadLine();
+                    break;
+                case 2:
+                    string pr = @"C:\Users\saivignesh\";
+                    string jr = @"C:\Users\saivignesh\Desktop\programmed\";
+                    Console.WriteLine("Enter File Names");
+                    string mr = Console.ReadLine();
+                    string rr=Console.ReadLine();
+                    string tr = string.Concat(pr,mr);
+                    
+                    string hr = string.Concat(jr,rr);
+                    File.Copy(tr,hr);
+                    FileInfo gr = new FileInfo(tr);
+                    Console.WriteLine(gr.Extension);
+                    Console.WriteLine(gr.CreationTime);
+                    Console.WriteLine(gr.DirectoryName);
+                    break;
+                case 3:
+                    string sp = @"C:\Users\saivignesh\";
+                    Console.WriteLine("Enter File Names");
+                    string mp = Console.ReadLine();
+                    string tp = string.Concat(sp, mp);
+                    File.Delete(tp);
+                    break;
+                case 4:
+                    string st = @"C:\Users\saivignesh\";
+                    Console.WriteLine("Enter File Names");
+                    string mt = Console.ReadLine();
+                    string tt = string.Concat(st,mt);
+                    string pt = Console.ReadLine();
+                    string ht = string.Concat(st,pt);
+                    File.Move(tt, ht);
+                    FileInfo gt = new FileInfo(ht);
+                    Console.WriteLine(gt.Extension);
+                    Console.WriteLine(gt.CreationTime);
+                    Console.WriteLine(gt.DirectoryName);
+                    break;
+            }
         }
     }
 }
